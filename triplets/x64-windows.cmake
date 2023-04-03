a8d36bc1db5fed03300cdffc0ace0251282e700d
kaapomoi <56117900+kaapomoi@users.noreply.github.com>
2023-04-03 21:27:17 +0300

    Link assimp statically

--- triplets/x64-windows.cmake
+++ triplets/x64-windows.cmake
@@ -1,4 +1,9 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
-set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE dynamic)
 
+if(${PORT} MATCHES "assimp")
+	set(VCPKG_CRT_LINKAGE static)
+	set(VCPKG_LIBRARY_LINKAGE static)
+else()
+	set(VCPKG_CRT_LINKAGE dynamic)
+	set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
